27b252d6d86d1ee119beee1201985a157371cb1e
Kirellos200 <167705451+Kirellos200@users.noreply.github.com>
2025-04-15 20:30:45 +0200

    Update ComparePriority.h

--- Header Files/ComparePriority.h
+++ Header Files/ComparePriority.h
@@ -6,4 +6,11 @@ struct ComparePriority {
         return p1.getPriority() > p2.getPriority();
     }
 };
+struct CompareRemainingTime{
+    bool operator()(Process&p1,Process&p2)
+    {
+        return p1.getRemainingTime()>p2.getRemainingTime();
+    }
+
+};
 #endif // COMPARE_PRIORITY_H
